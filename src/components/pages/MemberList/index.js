c3412719e438d22102148f0e6a10405fb45575b8
Thauan Vargas <thauan@outlook.pt>
2020-01-18 03:06:33 +0000

    feat: Added Memberlist, with MaterialIcons for licensed users

--- src/components/pages/MemberList/index.js
+++ src/components/pages/MemberList/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Row, Col } from '../../../../node_modules/react-bootstrap';
 import { useTabletop } from '../../../hooks/useTableTop';
-import { ChallangeCard } from '../../ChallangeCard';
+import { MemberCard } from '../../MemberCard';
 
 const key = process.env.REACT_APP_MEMBERLIST_SHEET;
 
 export function Memberlist() {
-  const SHEET_DATA = useTabletop(key);
-
-  if (SHEET_DATA.length === 0) {
-    return <h1> Carregando... </h1>;
-  }
+  // const MEMBERS_LIST = useTabletop(key);
+  const MEMBERS_LIST = [
+    { nome: 'Thauan', level: 'Aprendiz', licenca: 'true' },
+    { nome: 'Goufix', level: 'Instrutor', licenca: 'false' }
+  ];
+  // console.log(MEMBERS_LIST);
+  // if (MEMBERS_LIST.length === 0) {
+  //   return <h1> Carregando... </h1>;
+  // }
 
   return (
     <>
       <Row>
-        {SHEET_DATA.map((sheetRow, index) => {
+        {MEMBERS_LIST.map((sheetRow, index) => {
           return (
             <Col sm={4} key={index}>
-              <ChallangeCard
+              <MemberCard
                 name={sheetRow.nome}
                 level={sheetRow.level}
                 licenca={sheetRow.licenca}
