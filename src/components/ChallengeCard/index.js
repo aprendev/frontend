9c49acdca6a5757f33765de0a1f7ca8c0e7588c1
Goufix <alifer.17.11@gmail.com>
2020-01-19 18:17:37 -0300

    Card Style splitting

--- src/components/ChallengeCard/index.js
+++ src/components/ChallengeCard/index.js
@@ -1,34 +1,39 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 export function ChallengeCard(props) {
   const { name, description, level } = props;
+
+  // styles
+  const cardStyle = {
+    marginBottom: '5px',
+    marginLeft: '5px',
+    backgroundColor: '#3269A8',
+    color: '#fff'
+  };
+  const cardBodyStyle = {
+    backgroundColor: '#fff',
+    color: '#000'
+  };
+  const cardFooterStyle = {
+    backgroundColor: '#fff',
+    color: '#000',
+    display: 'flex',
+    justifyContent: 'space-between'
+  };
+
   return (
     <>
-      <Card
-        style={{
-          marginBottom: '5px',
-          marginLeft: '5px',
-          backgroundColor: '#3269A8',
-          color: '#fff'
-        }}
-      >
+      <Card style={cardStyle}>
         <Card.Header>{name}</Card.Header>
-        <Card.Body
-          style={{
-            backgroundColor: '#fff',
-            color: '#000'
-          }}
-        >
+        <Card.Body style={cardBodyStyle}>
           <span>{description}</span>
         </Card.Body>
-        <Card.Footer
-          style={{
-            backgroundColor: '#fff',
-            color: '#000'
-          }}
-        >
+        <Card.Footer style={cardFooterStyle}>
           <span>{level}</span>
+          <Button variant="light">
+            <a href="https://forms.gle/X34tjMfEjnWEaawGA">Enviar atividade</a>
+          </Button>
         </Card.Footer>
       </Card>
     </>
