c29f686da34dbdbdd2841282b769e8c0d4a8a99e
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-01-21 13:54:31 -0300

    feat: Add border-radius to navbar

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -2,6 +2,7 @@ import { Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
 
 export const CustomNavbar = styled(Navbar)`
+  border-radius: 0 0 0.25rem 0.25rem;
   background-color: #3269a8;
   font-weight: bold;
 `;
