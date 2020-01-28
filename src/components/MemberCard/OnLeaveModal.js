813552af888531c67660db06dede9cb403d4dca6
Alifer de Medeiros Quevedo <alifer.17.11@gmail.com>
2020-01-27 21:57:20 -0300

    Revert "Aumentado o tamanho do modal"

--- src/components/MemberCard/OnLeaveModal.js
+++ src/components/MemberCard/OnLeaveModal.js
@@ -4,11 +4,7 @@ import { Modal } from 'react-bootstrap';
 export function OnLeaveModal(props) {
   return (
     <>
-      <Modal
-        size="lg"
-        show={props.show}
-        onHide={() => props.handleCloseModal()}
-      >
+      <Modal show={props.show} onHide={() => props.handleCloseModal()}>
         <Modal.Header closeButton>Status da licença:</Modal.Header>
         <Modal.Body>
           Este membro está de licença pelo período:{' '}
