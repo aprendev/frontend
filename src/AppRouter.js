ac98e1c9c2257a8d28ee8520bcff837d784e43c1
Goufix <alifer.17.11@gmail.com>
2020-01-31 21:00:23 -0300

    Adiciona rotas protegidas admim/auth

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { AdminRoute } from './components/AdminRoute';
+import { AuthRoute } from './components/AuthRoute';
 import { useLocationTracker } from './hooks/useLocationTracker';
+import { Admin } from './pages/Admin';
 import { Home } from './pages/Home';
+import { Login } from './pages/Login';
 import { Memberlist } from './pages/MemberList';
 import { Presentation } from './pages/Presentation';
 import { Trap } from './pages/Trap';
@@ -13,17 +17,13 @@ export function AppRouter() {
   return (
     <Switch>
       <Route exact path="/">
-        <Presentation />
+        <Login />
       </Route>
-      <Route exact path="/admin.php">
-        <Trap />
-      </Route>
-      <Route exact path="/Challenges">
-        <Home />
-      </Route>
-      <Route exact path="/memberlist">
-        <Memberlist />
-      </Route>
+      <AuthRoute exact path="/home" component={() => <Presentation />} />
+      <AuthRoute exact path="/admin.php" component={() => <Trap />} />
+      <AuthRoute exact path="/challenges" component={() => <Home />} />
+      <AuthRoute exact path="/memberlist" component={() => <Memberlist />} />
+      <AdminRoute exact path="/admin" component={() => <Admin />} />
     </Switch>
   );
 }
---
+++ src/components/AdminRoute/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { isAuthenticated } from '../../services/auth';
+
+export const AdminRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+      )
+    }
+  />
+);
