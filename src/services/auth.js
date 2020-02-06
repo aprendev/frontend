4ba904cc1ba901c1193ab14daa7cf1282f3f2102
Goufix <alifer.17.11@gmail.com>
2020-02-05 22:23:45 -0300

    Faz com que a função retorne o token

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,8 @@
 export const TOKEN_KEY = 'AUTH_TOKEN';
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+export const isAuthenticated = () => {
+  const token = localStorage.getItem(TOKEN_KEY);
+  return !!token; // token !== null -- True
+};
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = (token) => {
   localStorage.setItem(TOKEN_KEY, token);
