de4d64832d47feb945476ec4f22fdb7004e89aa1
Goufix <alifer.17.11@gmail.com>
2020-02-05 22:24:07 -0300

    Refatora a autenticação

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,28 +1,45 @@
 import React, { useState } from 'react';
-import { Col, Row, Form, Button } from 'react-bootstrap';
+import { Col, Row, Form, Button, Alert } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import { api } from '../../services/api';
 import { login } from '../../services/auth';
 
-export function Login() {
+export function Login(props) {
   const history = useHistory();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState(false);
+
+  // useEffect(() => {
+  //   if (props.location.state.error) {
+  //     return <h1>You are not logged</h1>;
+  //   }
+  // }, [props.location.state]);
 
   async function handleSubmit(event) {
     event.preventDefault();
-    const { data } = await api.post('/auth', {
-      username,
-      password
-    });
-    login(data.token);
-    history.push('/home');
+    await api
+      .post('/auth', {
+        username,
+        password
+      })
+      .then(({ data }) => {
+        login(data.tokens.token);
+        history.push('/home');
+      })
+      .catch((err) => {
+        const errorMessage = err.response.data.message;
+        const error = errorMessage
+          .substr(errorMessage.indexOf(': '), 1000)
+          .replace(': ', '');
+        console.log(error);
+        setLoginError(error);
+      });
   }
 
   return (
     <>
       <Row>
-        <Col />
         <Col bsPrefix="col d-flex justify-content-center">
           {/* Form down here */}
           <Form onSubmit={handleSubmit}>
@@ -42,12 +59,16 @@ export function Login() {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </Form.Group>
+            {loginError && (
+              <>
+                <Alert variant="danger">{loginError}</Alert>
+              </>
+            )}
             <Button variant="primary" type="submit">
               Login
             </Button>
           </Form>
         </Col>
-        <Col />
       </Row>
     </>
   );
