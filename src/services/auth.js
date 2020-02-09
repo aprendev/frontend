a424973ba4aaa39c0aa05e23ca09c557b32561ff
Goufix <alifer.17.11@gmail.com>
2020-02-09 16:15:57 -0300

    Faz a validação do token no backend

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,17 @@
+// import jwtDecode from 'jwt-decode';
+import { api } from './api';
+
 export const TOKEN_KEY = 'AUTH_TOKEN';
-export const isAuthenticated = () => {
-  const token = localStorage.getItem(TOKEN_KEY);
-  return !!token; // token !== null -- True
+export const isAuthenticated = async () => {
+  const token = getToken();
+  const { data } = await api.get('/validateToken', token);
+  console.log(data.isValidToken);
+  if (data.isValidToken) {
+    return true;
+  } else {
+    logout();
+    return false;
+  }
 };
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = (token) => {
