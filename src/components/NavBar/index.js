710db8b2655814625e35169683055107d785434f
Goufix <alifer.17.11@gmail.com>
2020-02-09 16:31:37 -0300

    Adiciona o botão logout

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { isAuthenticated } from '../../services/auth';
 import { CustomNavbar } from './styles';
 
 export function NavBar() {
+  const [isAutorizedUser, setIsAutorizedUser] = useState(false);
+
+  useEffect(() => {
+    async function validateAuth() {
+      const response = await isAuthenticated();
+      setIsAutorizedUser(response);
+    }
+    validateAuth();
+  });
+
+  const linkStyle = { color: '#fff', padding: 15 };
+
   return (
     <CustomNavbar variant="dark" expand="lg" className="mb-3">
       <Container>
@@ -11,16 +24,23 @@ export function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Link style={{ color: '#fff', padding: 15 }} to="/home">
+            <Link style={linkStyle} to="/home">
               Home
             </Link>
-            <Link style={{ color: '#fff', padding: 15 }} to="/challenges">
+            <Link style={linkStyle} to="/challenges">
               Desafios
             </Link>
-            <Link style={{ color: '#fff', padding: 15 }} to="/memberlist">
+            <Link style={linkStyle} to="/memberlist">
               Lista de membros
             </Link>
           </Nav>
+          {isAutorizedUser && (
+            <Nav className="ml-auto">
+              <Link style={linkStyle} to="/logout">
+                Sair
+              </Link>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Container>
     </CustomNavbar>
