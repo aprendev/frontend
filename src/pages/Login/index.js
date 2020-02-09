f47ce45dba24991ecfa2d08c3848446443821c8f
Goufix <alifer.17.11@gmail.com>
2020-02-09 16:18:44 -0300

    Não exibir a página caso usuário esteja logado

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,23 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col, Row, Form, Button, Alert } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import { api } from '../../services/api';
-import { login } from '../../services/auth';
+import { login, isAuthenticated } from '../../services/auth';
 
 export function Login() {
   const history = useHistory();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loginError, setLoginError] = useState(false);
+  const [isAutorizedUser, setIsAutorizedUser] = useState(false);
 
-  // useEffect(() => {
-  //   if (props.location.state.error) {
-  //     return <h1>You are not logged</h1>;
-  //   }
-  // }, [props.location.state]);
+  useEffect(() => {
+    async function validateAuth() {
+      const response = await isAuthenticated();
+      setIsAutorizedUser(response);
+    }
+    validateAuth();
+    if (isAutorizedUser) {
+      history.push('/home');
+    }
+  }, [history, isAutorizedUser]);
 
   async function handleSubmit(event) {
     event.preventDefault();
+
+    function loginError(err) {
+      try {
+        const errorMessage = err.response.data.message;
+        const error = errorMessage
+          .substr(errorMessage.indexOf(': '), 1000)
+          .replace(': ', '');
+        setLoginError(error);
+      } catch {
+        setLoginError('Erro desconhecido');
+      }
+    }
+
     await api
       .post('/auth', {
         username,
@@ -27,49 +46,44 @@ export function Login() {
         login(data.tokens.token);
         history.push('/home');
       })
-      .catch((err) => {
-        const errorMessage = err.response.data.message;
-        const error = errorMessage
-          .substr(errorMessage.indexOf(': '), 1000)
-          .replace(': ', '');
-        console.log(error);
-        setLoginError(error);
-      });
+      .catch((err) => loginError(err));
   }
 
   return (
     <>
-      <Row>
-        <Col bsPrefix="col d-flex justify-content-center">
-          {/* Form down here */}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group>
-              <Form.Label>Usuário:</Form.Label>
-              <Form.Control
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Senha:</Form.Label>
-              <Form.Control
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
-            {loginError && (
-              <>
-                <Alert variant="danger">{loginError}</Alert>
-              </>
-            )}
-            <Button variant="primary" type="submit">
-              Login
-            </Button>
-          </Form>
-        </Col>
-      </Row>
+      {!isAutorizedUser && (
+        <Row>
+          <Col bsPrefix="col d-flex justify-content-center">
+            {/* Form down here */}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Label>Usuário:</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Senha:</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              {loginError && (
+                <>
+                  <Alert variant="danger">{loginError}</Alert>
+                </>
+              )}
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
