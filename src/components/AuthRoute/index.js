5f2c4ad4ad567846e2a82c00da54ae866e26228d
Goufix <alifer.17.11@gmail.com>
2020-02-09 16:16:57 -0300

    Adiciona a validação de correta autenticação

--- src/components/AuthRoute/index.js
+++ src/components/AuthRoute/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { isAuthenticated } from '../../services/auth';
 
-export const AuthRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      isAuthenticated() ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{ pathname: '/', state: { from: props.location, error: 'Erro' } }}
-        />
-      )
+export const AuthRoute = ({ component: Component, ...rest }) => {
+  const [isAutorizedUser, setIsAutorizedUser] = useState(true);
+
+  useEffect(() => {
+    async function validateAuth() {
+      const response = await isAuthenticated();
+      setIsAutorizedUser(response);
     }
-  />
-);
+    validateAuth();
+  });
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAutorizedUser ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/',
+              state: { from: props.location, error: 'Erro' }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
